524a75664466558c9e2f15fe3cd631d2c8ab2183
b2marshall <b2marshall@yahoo.com>
2021-12-24 17:02:21 -0700

    outlines a class for user

---
+++ weighttracker.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+have_not_eaten_yet = ['calories':0,'protein':0,'fats':0,'carbs':0,]
+
+class user: 
+    
+	def __init__(self, name, daily_weight, daily_macros): 
+		self.name = name
+		self.weight = weight
+		self.daily_macros = daily_macros
+		
+		
